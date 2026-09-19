0546090b00b384261516235780cb1ee8cb6b9f86
kairosscommand-blip <kairosscommand@gmail.com>
2026-09-19 14:28:06 -0700

    Delete src/main directory

--- src/main/java/com/nexuscraft/nexusdungeons/LootItem.java
+++
@@ -1,7 +0,0 @@
-package com.nexuscraft.nexusdungeons;
-
-import org.bukkit.Material;
-
-/** One weighted entry in a loot tier -- see {@link DungeonLootTable}. */
-record LootItem(Material material, int min, int max, int weight) {
-}
